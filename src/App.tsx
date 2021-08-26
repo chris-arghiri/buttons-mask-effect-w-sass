253293623d50d794e590b6ba7f1eb9d8684610ef
chris-arghiri <chris.arghiri23@gmail.com>
2021-08-26 18:07:14 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='ButtonContainer1'>
+        <span className='Mask'>Text Here</span>
+        <button type='button' name='Hover'>
+          Text Here
+        </button>
+      </div>
+      <div className='ButtonContainer2'>
+        <span className='Mask'>Text Here</span>
+        <button type='button' name='Hover'>
+          Text Here
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
